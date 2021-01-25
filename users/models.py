89b68f72ab4bd5935b0ef4865ba86b397b2481d7
Stephen <stephen.shoben@gmail.com>
2021-01-25 16:39:02 -0500

    Completed Profile update - Section 9

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  image = models.ImageField( default='default.png', upload_to = 'profile_pics')
+
+  def __str__(self):
+    return f'{self.user.username} Profile'
+
+  def save(self):
+    super().save()
+
+    img = Image.open(self.image.path)
+
+    IMG_MAX = 150
+
+    if img.height > IMG_MAX or img.width > IMG_MAX:
+      output_size = (IMG_MAX, IMG_MAX)
+      img.thumbnail(output_size)
+      img.save(self.image.path)
